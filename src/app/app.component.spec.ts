88034f1f4324a74d7ec2482c9448c41bf10b0db2
bruno aguilar <josebrunoaguilar@gmail.com>
2019-08-19 11:59:52 -0400

    pruebas con las condiciones

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,6 +4,7 @@ import { GameOfLife } from './models/gameoflife';
 
 describe('Game of life', () => {
   const game = new GameOfLife();
+
     it('Esta vivo posicion 1,2', () => {
       const x = 1;
       const y = 2;
@@ -18,6 +19,50 @@ describe('Game of life', () => {
 
       const resul = game.celulaMuerta(x,y);
       expect(resul).toEqual(true);
+    });
+
+    it('Celula muerta vecino 0', () =>{
+      const x = 1;
+      const y = 0;
+
+      const resul = game.cantidadVecinos(x,y);
+      expect(resul).toEqual(0);
+    });
+    //condicion 1
+    it('Cualquier celula viva con menos de dos vecinas vivas, muere', () => {
+      const x = 1;
+      const y = 2;
+
+      const resul = game.cantidadVecinos(x,y);
+      expect(resul).toEqual(1);
+    });
+    //condicion 2
+    it('Cualquier celula viva con dos o tres vecinas vivas, sobrevive en la proxima generacion', () =>{
+      const x = 2;
+      const y = 2;
+
+      const resul = game.cantidadVecinos(x,y);
+      expect(resul).toEqual(2);
+    });
+    //condicion 4
+    it('Cualquier celula muerta con exactamente tres vecinas vivas, cobra vida en la siguiente generacion', () =>{
+      const x = 2;
+      const y = 1;
+
+      const resul = game.cantidadVecinos(x,y);
+      expect(resul).toEqual(3);
+    });
+
+    //agregue otro cela para probar la condicion 3
+    let gameTwo = new GameOfLife();
+    gameTwo.board = gameTwo.DevolverArray2();
+
+    it('Cualquier celula viva con mas de tres vecinas vivas, muere', () =>{
+      const x = 2;
+      const y = 3;
+
+      const resul = gameTwo.cantidadVecinos(x,y);
+      expect(resul).toEqual(3);
     });
 });
 
--- src/app/models/gameoflife.ts
+++ src/app/models/gameoflife.ts
@@ -1,5 +1,3 @@
-
-
 export class GameOfLife {
 
    public board: number[][] = [ [0, 0, 0, 0, 0],
@@ -21,6 +19,13 @@ export class GameOfLife {
     public DevolverArray() {
         return this.board;
     }
+    public DevolverArray2(){
+     return  this.board = [  [0, 0, 0, 0, 0],
+                            [0, 0, 1, 0, 0],
+                            [0, 0, 1, 1, 0],
+                            [0, 0, 1, 0, 0],
+                            [0, 0, 0, 0, 0]];
+    }
 
 
     public celulaViva(row: number, col: number){
@@ -28,5 +33,69 @@ export class GameOfLife {
     }
     public celulaMuerta(row: number, col: number){
         return this.board[row][col] === 0;
+    }
+    private verificarPosicion(x: number,y: number) {
+      return x >= 0 && y >= 0 && x < 5 && y < 5;
+    }
+    public cantidadVecinos( row: number, column: number){
+
+      let cant = 0 ;
+      let x = row - 1;
+      let y = column;
+      if(this.verificarPosicion(x,y)){
+         if(this.celulaViva(x,y))
+           cant++;
+      }
+
+      x = row - 1;
+      y = column -1;
+      if(this.verificarPosicion(x,y)){
+         if(this.celulaViva(x,y))
+           cant++;
+      }
+
+      x = row - 1;
+      y = column + 1;
+      if(this.verificarPosicion(x,y)){
+         if(this.celulaViva(x,y))
+           cant++;
+      }
+
+      x = row;
+      y = column + 1;
+      if(this.verificarPosicion(x,y)){
+         if(this.celulaViva(x,y))
+           cant++;
+      }
+
+      x = row + 1;
+      y = column + 1;
+      if(this.verificarPosicion(x,y)){
+         if(this.celulaViva(x,y))
+           cant++;
+      }
+
+      x = row + 1;
+      y = column;
+      if(this.verificarPosicion(x,y)){
+         if(this.celulaViva(x,y))
+           cant++;
+      }
+
+      x = row + 1;
+      y = column -1;
+      if(this.verificarPosicion(x,y)){
+         if(this.celulaViva(x,y))
+           cant++;
+      }
+
+      x = row;
+      y = column -1;
+      if(this.verificarPosicion(x,y)){
+         if(this.celulaViva(x,y))
+           cant++;
+      }
+
+        return cant;
     }
 }
